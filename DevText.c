e95922a1d0adf59ba90f5165860861554061aa15
yu8023only <143868199+yu8023only@users.noreply.github.com>
2023-10-06 17:24:09 +0800

    added a file

---
+++ DevText.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+	//比较大小
+	/*int a, b, c;
+	printf("请输入三个大小不同的数：");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	int max = 0;
+	if( a>b ){
+		if( a>c ){
+			max = a;
+		}else{
+			max = c;
+		}
+	}else{
+		if( b>c ){
+			max = b;
+		}else{
+			max = c;
+		}
+	}
+	printf("这三个整数中最大的是%d\n", max);*/
+	
+	printf("* * * *\n");
+	printf(" * * *\n");
+	printf("  * *\n");
+	printf("   *\n");
+	
+	return 0;
+}
